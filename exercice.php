0826f56c3e3ee2bd45cbcb2a6a9cd7af9fa19a96
karen-ifrt <karen-ifrt@users.noreply.github.com>
2019-11-14 16:58:33 +0100

    v2

--- exercice.php
+++ exercice.php
@@ -37,11 +37,7 @@ session_start();
 
     </div>
 
-    <form action="exercice.php" method="post" role="form">
-        <input type="number" name="reponse">
-        <input type="submit" name="form" value="VALIDER">
 
-    </form>
 
     <?php
 
@@ -50,13 +46,36 @@ session_start();
         $value = $_POST["reponse"];
         $result = $_SESSION['number'] * $_SESSION['random'];
         if ($value == $result) {
+            if (isset($_POST["reponse"])) {
+                $number = $_SESSION['number'];
+                echo '<h1>Table de ' . $number . '</h1>';
+                $random = rand(1, 10);
+                $result = $number * $random;
+                $_SESSION['random'] = $random;
+                $_SESSION['number'] = $number;
+                echo $number . " x " . $random . "<br>";
+            }
             echo 'Bonne réponse';
         } else {
+            if (isset($_POST["reponse"])) {
+                $number = $_SESSION['number'];
+                echo '<h1>Table de ' . $number . '</h1>';
+                $random = rand(1, 10);
+                $result = $number * $random;
+                $_SESSION['random'] = $random;
+                $_SESSION['number'] = $number;
+                echo $number . " x " . $random . "<br>";
+            }
             echo 'Mauvaise réponse';
         }
     }
 
     ?>
+
+    <form action="exercice.php" method="post" role="form">
+        <input type="number" name="reponse">
+        <input type="submit" name="form" value="VALIDER">
+    </form>
 
     <a href="revision.php">RETOUR</a>
 
