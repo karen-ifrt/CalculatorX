938f3e0430d879d0bb10d7256bd0462ae903dc2c
arnauddrm <arnaud.drm10@gmail.com>
2019-11-15 15:30:47 +0100

    khlsqfcoiel

--- index.php
+++ index.php
@@ -4,12 +4,42 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Tables de multiplication</title>
+    <link rel="stylesheet" href="assets/style.css">
+    <link rel="icon" href="img/favicon.png">
+    <title>CALCULATOR X</title>
 </head>
 <body>
 
-<a href="classique.php">Mode apprentissage</a>
-<a href="revision.php">Mode révision</a>
+    <img src="img/fond.png" id="fond" alt="fond">
+
+
+        <div class="title">
+            <h2>CALCU</h2>
+            <img src="img/logo-calcX.png" class="logo" alt="logo">
+            <h2>LATOR</h2>
+        </div>
+
+        
+        
+        <div class="menu">
+            <div class="btn1">
+                <img src="img/btn.png" class="btn-mode" alt="btn-mode">
+                <a href="classique.php" class="btn1">CLASSIQUE</a>
+            </div>
+            <div class="btn2">
+                <img src="img/btn.png" class="btn-mode" alt="btn-mode">
+                <a href="revision.php" class="btn2">ENTRAÎNEMENT</a>
+            </div>
+            
+            
+        </div>
+
+    
+
+
+    
+    
+
 
     
 </body>
